219bb21078ac2e1aa3ef79b943f3d51e42a7916d
Anastasiia Stos <anastasiiastos@Anastasiias-MacBook-Pro.local>
2022-04-10 23:35:37 +0200

    Add CRUD for book app

--- book/urls.py
+++ book/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from book.views import BookListView, BookDetailView, UnorderedBookView, FilterBooksView, SortedBookView, create_book, edit_book
+from book import views
 
 urlpatterns = [
-    path('all/', BookListView.as_view(), name="all_books"),
-    path('available/', UnorderedBookView.as_view(), name="unordered_books"),
-    path('filter/', FilterBooksView.as_view(), name="filter_books"),
-    path('sorted/', SortedBookView.as_view(), name="sorted_books"),
-    path('<int:pk>/', BookDetailView.as_view(), name="book_detail"),
-    path('new/', create_book, name='create_book'),
-    path('<int:pk>/edit/', edit_book, name='edit_book'),
+    path('/', views.book_list),
+    path('/<int:pk>/', views.book_detail),
 ]
